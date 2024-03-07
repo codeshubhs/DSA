aaac08543bd1b584d02320fa8e8e96af9199f28e
Shubham_Yadav <121845989+codeshubhs@users.noreply.github.com>
2024-03-07 16:47:15 +0530

    Time: 30 ms (10.21%), Space: 23.1 MB (13.91%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,12 +2,23 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
        int n=nums.size();
-int xorr=0;
+/*int xorr=0;
        for(int i=0; i<n; i++){
 xorr=xorr^nums[i];
        }
-       return xorr; 
+       return xorr; */
         
-       
+        map<int,int>mp;
+        
+        for(int i=0; i<n; i++){
+            mp[nums[i]]++;
+        }
+        
+        for(auto it:mp){
+            
+            if(it.second==1)
+                return it.first;
+        }
+       return -1;
     }
 };
